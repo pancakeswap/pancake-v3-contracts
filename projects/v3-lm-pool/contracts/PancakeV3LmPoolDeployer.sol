1454125e30233559c6f9f484e7e1961c855f5e9b
ChefMist <mist@pancakeswap.com>
2024-02-28 16:01:21 +0800

    feat: update mcv3 and related contracts

--- projects/v3-lm-pool/contracts/PancakeV3LmPoolDeployer.sol
+++ projects/v3-lm-pool/contracts/PancakeV3LmPoolDeployer.sol
@@ -5,13 +5,23 @@ import '@pancakeswap/v3-core/contracts/interfaces/IPancakeV3Factory.sol';
 import '@pancakeswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol';
 
 import './PancakeV3LmPool.sol';
+import './interfaces/IMasterChefV3.sol';
 
 /// @dev This contract is for Master Chef to create a corresponding LmPool when
 /// adding a new farming pool. As for why not just create LmPool inside the
 /// Master Chef contract is merely due to the imcompatibility of the solidity
 /// versions.
 contract PancakeV3LmPoolDeployer {
+    struct Parameters {
+        address pool;
+        address masterChef;
+    }
+
+    Parameters public parameters;
+
     address public immutable masterChef;
+
+    event NewLMPool(address indexed pool, address indexed LMPool);
 
     modifier onlyMasterChef() {
         require(msg.sender == masterChef, "Not MC");
@@ -24,8 +34,17 @@ contract PancakeV3LmPoolDeployer {
 
     /// @dev Deploys a LmPool
     /// @param pool The contract address of the PancakeSwap V3 pool
-    function deploy(IPancakeV3Pool pool) external onlyMasterChef returns (IPancakeV3LmPool lmPool) {
-        lmPool = new PancakeV3LmPool(address(pool), masterChef, uint32(block.timestamp));
-        IPancakeV3Factory(INonfungiblePositionManager(IMasterChefV3(masterChef).nonfungiblePositionManager()).factory()).setLmPool(address(pool), address(lmPool));
+    function deploy(address pool) external onlyMasterChef returns (address lmPool) {
+        parameters = Parameters({pool: pool, masterChef: masterChef});
+
+        lmPool = address(new PancakeV3LmPool{salt: keccak256(abi.encode(pool, masterChef, block.timestamp))}());
+
+        delete parameters;
+
+        // Set new LMPool for pancake v3 pool.
+        IPancakeV3Factory(INonfungiblePositionManager(IMasterChefV3(masterChef).nonfungiblePositionManager()).factory())
+            .setLmPool(pool, lmPool);
+
+        emit NewLMPool(pool, lmPool);
     }
 }
